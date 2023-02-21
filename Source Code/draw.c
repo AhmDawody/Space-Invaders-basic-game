e6087cb917a1e320f02a538700f2eaa0fd6f8cc1
Ahm.Dawody <107086104+AhmDawody@users.noreply.github.com>
2023-02-21 20:58:13 +0200

    Update draw.c

--- Source Code/draw.c
+++ Source Code/draw.c
@@ -18,10 +18,10 @@
 // Structs
 // --------
 typedef struct State{
-  uint8_t x;      								// x coordinate
-  uint8_t y;      								// y coordinate
-  const uint8_t *image; 					// ptr->image
-  uint32_t life;           	 			// 0=dead, 1=alive
+  uint8_t x;      				// x coordinate
+  uint8_t y;      				// y coordinate
+  const uint8_t *image; 			// ptr->image
+  uint32_t life;           	 		// 0=dead, 1=alive
 }STyp;
 
 // Externs
@@ -50,14 +50,14 @@ extern uint8_t count;
 void Draw_welcome(void){
 	Nokia5110_ClearBuffer();
 	Nokia5110_Clear();
-  Nokia5110_SetCursor(3, 0);
-  Nokia5110_OutString("SPACE");
-  Nokia5110_SetCursor(2, 1);
-  Nokia5110_OutString("INVADERS");
-  Nokia5110_SetCursor(1, 3);
-  Nokia5110_OutString("Press any");
+  	Nokia5110_SetCursor(3, 0);
+  	Nokia5110_OutString("SPACE");
+  	Nokia5110_SetCursor(2, 1);
+  	Nokia5110_OutString("INVADERS");
+ 	Nokia5110_SetCursor(1, 3);
+  	Nokia5110_OutString("Press any");
 	Nokia5110_SetCursor(0, 4);
-  Nokia5110_OutString("key to start");
+  	Nokia5110_OutString("key to start");
 	while(!(GPIO_PORTE_DATA_R&0x01)  && !(GPIO_PORTE_DATA_R&0x02) );
 	Delay100ms(2);
 }
@@ -68,19 +68,19 @@ void Draw_score(void){int8_t s;
 	s = count +'0';
 	Nokia5110_ClearBuffer();
 	Nokia5110_Clear();
-  Nokia5110_SetCursor(2, 1);
-  Nokia5110_OutString("GAME OVER");
-  Nokia5110_SetCursor(1, 3);
-  Nokia5110_OutString("Your Score");
-  Nokia5110_SetCursor(5, 5);
-  Nokia5110_OutString(&s);
+  	Nokia5110_SetCursor(2, 1);
+  	Nokia5110_OutString("GAME OVER");
+  	Nokia5110_SetCursor(1, 3);
+  	Nokia5110_OutString("Your Score");
+  	Nokia5110_SetCursor(5, 5);
+  	Nokia5110_OutString(&s);
 	Delay100ms(10);
 	Nokia5110_ClearBuffer();
 	Nokia5110_Clear();
 	Nokia5110_SetCursor(1, 1);
 	Nokia5110_OutString("Press any");
 	Nokia5110_SetCursor(0, 3);
-  Nokia5110_OutString("key to start");
+  	Nokia5110_OutString("key to start");
 	while(!(GPIO_PORTE_DATA_R&0x01)  && !(GPIO_PORTE_DATA_R&0x02));
 	Delay100ms(2);
 }
@@ -91,19 +91,19 @@ void Draw_victory(void){int8_t s;
 	s = count +'0';
 	Nokia5110_ClearBuffer();
 	Nokia5110_Clear();
-  Nokia5110_SetCursor(2, 1);
-  Nokia5110_OutString("Victory!!");
-  Nokia5110_SetCursor(1, 3);
-  Nokia5110_OutString("Your Score");
-  Nokia5110_SetCursor(5, 5);
-  Nokia5110_OutString(&s);
+  	Nokia5110_SetCursor(2, 1);
+  	Nokia5110_OutString("Victory!!");
+  	Nokia5110_SetCursor(1, 3);
+  	Nokia5110_OutString("Your Score");
+  	Nokia5110_SetCursor(5, 5);
+  	Nokia5110_OutString(&s);
 	Delay100ms(10);
 	Nokia5110_ClearBuffer();
 	Nokia5110_Clear();
 	Nokia5110_SetCursor(1, 1);
 	Nokia5110_OutString("Press any");
 	Nokia5110_SetCursor(0, 3);
-  Nokia5110_OutString("key to start");
+  	Nokia5110_OutString("key to start");
 	while(!(GPIO_PORTE_DATA_R&0x01)  && !(GPIO_PORTE_DATA_R&0x02));
 	Delay100ms(2);
 }
@@ -111,53 +111,53 @@ void Draw_victory(void){int8_t s;
 // Draw Sprites updates
 // ---------------------
 void Draw(void){uint8_t i;
-		Nokia5110_ClearBuffer();			// Clear buffer	
-		// Draw alive enemies
-		for(i=0;i<4;i++){
-				if(Enemy[i].life > 0){
-						Nokia5110_PrintBMP(Enemy[i].x, Enemy[i].y, Enemy[i].image, 0);
-						if(Enemy[i].image == SmallExplosion0){
-								Enemy[i].life = 0;
-								Enemy[i].x = 100;
-								Enemy[i].y = 100;
-						}
-				}
+	Nokia5110_ClearBuffer();			// Clear buffer	
+	// Draw alive enemies
+	for(i=0;i<4;i++){
+		if(Enemy[i].life > 0){
+			Nokia5110_PrintBMP(Enemy[i].x, Enemy[i].y, Enemy[i].image, 0);
+			if(Enemy[i].image == SmallExplosion0){
+					Enemy[i].life = 0;
+					Enemy[i].x = 100;
+					Enemy[i].y = 100;
+			}
 		}
-		// Draw alive player ship
-		if(Ship.life > 0){
-				Nokia5110_PrintBMP(Ship.x, Ship.y, Ship.image, 0);
-				if(Ship.image == BigExplosion0){
-									Ship.life = 0;
-									Ship.x = 100;
-									Ship.y = 100;
-				}
-		}	
-		// Draw alive  normal missile		
-		if(n_missile.life > 0){
-				Nokia5110_PrintBMP(n_missile.x, n_missile.y, n_missile.image, 0);
+	}
+	// Draw alive player ship
+	if(Ship.life > 0){
+			Nokia5110_PrintBMP(Ship.x, Ship.y, Ship.image, 0);
+			if(Ship.image == BigExplosion0){
+					Ship.life = 0;
+					Ship.x = 100;
+					Ship.y = 100;
+			}
+	}	
+	// Draw alive  normal missile		
+	if(n_missile.life > 0){
+			Nokia5110_PrintBMP(n_missile.x, n_missile.y, n_missile.image, 0);
+	}
+	// Draw alive bunker
+	if(Bunker.life > 0){
+			Nokia5110_PrintBMP(Bunker.x, Bunker.y, Bunker.image, 0);
+			if(Bunker.image == SmallExplosion0){
+					Bunker.life = 0;
+					Bunker.x = 100;
+					Bunker.y = 100;
+			}			
+	}
+	// Draw special missile	
+	if(missile[0].life > 0){
+			Nokia5110_PrintBMP(missile[0].x, missile[0].y, missile[0].image, 0);		
+	}
+	if(missile[1].life > 0){
+			Nokia5110_PrintBMP(missile[1].x, missile[1].y, missile[1].image, 0);		
 		}
-		// Draw alive bunker
-		if(Bunker.life > 0){
-				Nokia5110_PrintBMP(Bunker.x, Bunker.y, Bunker.image, 0);
-				if(Bunker.image == SmallExplosion0){
-							Bunker.life = 0;
-							Bunker.x = 100;
-							Bunker.y = 100;
-				}			
-		}
-		// Draw special  normal missile	
-		if(missile[0].life > 0){
-				Nokia5110_PrintBMP(missile[0].x, missile[0].y, missile[0].image, 0);		
-		}
-		if(missile[1].life > 0){
-				Nokia5110_PrintBMP(missile[1].x, missile[1].y, missile[1].image, 0);		
-		}
-		// Draw alive  enemy missile	
-		if(laser.life > 0){
-				Nokia5110_PrintBMP(laser.x, laser.y, laser.image, 0);		
-		}
-		if(laser1.life > 0){
-				Nokia5110_PrintBMP(laser1.x, laser1.y, laser1.image, 0);		
-		}
-		Nokia5110_DisplayBuffer();      // draw buffer
+	// Draw alive  enemy missile	
+	if(laser.life > 0){
+			Nokia5110_PrintBMP(laser.x, laser.y, laser.image, 0);		
+	}
+	if(laser1.life > 0){
+			Nokia5110_PrintBMP(laser1.x, laser1.y, laser1.image, 0);		
+	}
+	Nokia5110_DisplayBuffer();      // draw buffer
 }
